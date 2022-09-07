43e5d3a7603b4cc54d22120264c8f5ff0e6f4cc0
Shekhar Kumar <shekharkumar235838@gmail.com>
2022-09-07 19:00:45 +0530

    minimal setup done

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Button = React.forwardRef((props,ref) => {
     return(
-        <button onClick={props.onClick} className="border-none rounded  bg-gray-900 text-white font-semibold px-5 py-3" ref={ref}>
+        <button onClick={props.onClick} {...props} className="border-none min-w-max rounded  bg-gray-900 text-white font-semibold px-5 py-3" ref={ref}>
             {props.text ? props.text :  'CLICK ME'}
         </button>
     )
